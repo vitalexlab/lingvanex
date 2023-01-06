d3a17bbfe3ffe8b264c3eef7f87e0dff807c003b
Varabyeu <v.varabyeu.exlab@gmail.com>
2023-01-06 18:51:53 +0300

    initial

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from utils.managet import run_parser
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/")
+def hello_world():
+    parser = run_parser('https://apps.microsoft.com/store/category/Business')
+    host = parser.host
+    return f"<a href='{host}'>{host}</p>"
